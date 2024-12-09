140ffda255d44602c73a82ff387c9696a6a83a8d
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-12-09 13:51:13 +0530

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -99,7 +99,7 @@ function compress(req, res, input) {
           res.setHeader("x-original-size", req.params.originSize);
           res.setHeader("x-bytes-saved", req.params.originSize - info.size);
           res.statusCode = 200;
-        });
+        })
         .on("data", chunk => {
           res.write(chunk);
         })

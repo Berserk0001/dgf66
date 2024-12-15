5c98d11e69ac7cc308ba36ef2c5d4e1cfb067a3c
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-12-15 10:14:28 +0530

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -66,7 +66,7 @@ function redirect(req, res) {
  * @param {http.ServerResponse} res - The HTTP response.
  * @param {http.IncomingMessage} input - The input stream for image data.
  */
-function compress(req, res, input) {
+/*function compress(req, res, input) {
   const format = req.params.webp ? "webp" : "jpeg";
   const sharpInstance = sharp({
     unlimited: true,
@@ -109,6 +109,26 @@ function compress(req, res, input) {
     });
 
   input.pipe(sharpInstance);
+}*/
+function compress(req, res, input) {
+    const format = req.params.webp ? 'webp' : 'jpeg';
+    sharp(input)
+        .grayscale(req.params.grayscale)
+        .toFormat(format, {
+            quality: req.params.quality,
+            progressive: true,
+            optimizeScans: true
+        })
+        .toBuffer((err, output, info) => {
+            if (err || !info || res.headersSent) return redirect(req, res);
+            res.setHeader('content-type', `image/${format}`);
+            res.setHeader('content-length', info.size);
+            res.setHeader('x-original-size', req.params.originSize);
+            res.setHeader('x-bytes-saved', req.params.originSize - info.size);
+            res.status(200);
+            res.write(output);
+            res.end()
+        })
 }
 
 /**

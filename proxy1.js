7cc0d076146cc9af757318015dc243470208f6cd
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-12-15 10:20:57 +0530

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -50,14 +50,15 @@ function copyHeaders(source, target) {
  * @param {http.ServerResponse} res - The HTTP response.
  */
 function redirect(req, res) {
-  if (!res.headersSent) {
-    res.writeHead(302, {
-      Location: encodeURI(req.params.url),
-      'Content-Length': '0'
-    });
-    ["cache-control", "expires", "date", "etag"].forEach(header => res.removeHeader(header));
-    res.end();
-  }
+  if (res.headersSent) return;
+
+  res.setHeader('content-length', 0);
+  res.removeHeader('cache-control');
+  res.removeHeader('expires');
+  res.removeHeader('date');
+  res.removeHeader('etag');
+  res.setHeader('location', encodeURI(req.params.url));
+  res.status(302).end();
 }
 
 /**
